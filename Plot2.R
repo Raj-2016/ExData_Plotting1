75522b06e4338c9f3ea363734d69391f02d2d466
Raj <rajvirdi@Gmail.com>
2017-02-14 14:44:05 -0600

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,51 @@
+# Plot Two
+
+
+# How much memory will it use:
+
+# MB - The dataset has 2,075,259 rows and 9 columns
+
+round(2075259 *9*8/2^{20},2)
+
+# set working directory
+
+setwd("~/Raj/R/household_power_consumption")
+
+# Load Data
+
+mydata <- read.csv("household_power_consumption.txt", sep = ";")
+
+
+# Convert date column to date type.
+
+mydata$Date <- as.Date(mydata$Date, format='%d/%m/%Y')
+
+# Subset data we need to use for plot
+
+UseData <- subset(mydata, (mydata$Date >= '2007-02-01'& mydata$Date <= '2007-02-02'))
+
+
+# Convert columns 3 to 9 to numeric and convert to Kilowatt
+UseData[,3:9] <- sapply(UseData[,3:9], as.numeric)
+
+UseData[3] <- sapply(UseData[3], function(x) x/1000)
+
+# Convert dates and times and add new column date time
+
+UseData$datetime <- strptime(paste(UseData$Date, UseData$Time), "%Y-%m-%d %H:%M:%S")
+
+UseData$datetime <- as.POSIXct(UseData$datetime)
+
+# Create Plot
+
+with (UseData, plot(Global_active_power ~ datetime, type = "l",
+                    ylab = "Global Active Power (kilowatts)", xlab = ""))
+
+# Create PNG file
+
+png(file = "plot2.png", width = 480, height = 480, units = "px")
+
+with (UseData, plot(Global_active_power ~ datetime, type = "l",
+                    ylab = "Global Active Power (kilowatts)", xlab = ""))
+
++dev.off()
---
+++ Plot4.R
@@ -0,0 +1,105 @@
+# Plot Four
+
+
+# How much memory will it use:
+
+# MB - The dataset has 2,075,259 rows and 9 columns
+
+round(2075259 *9*8/2^{20},2)
+
+# set working directory
+
+setwd("~/Raj/R/household_power_consumption")
+
+# Load Data
+
+mydata <- read.csv("household_power_consumption.txt", sep = ";")
+
+
+# Convert date column to date type.
+
+mydata$Date <- as.Date(mydata$Date, format='%d/%m/%Y')
+
+# Subset data we need to use for plot
+
+UseData <- subset(mydata, (mydata$Date >= '2007-02-01'& mydata$Date <= '2007-02-02'))
+
+
+# Convert columns 3 to 9 to numeric and convert to Kilowatt
+UseData[,3:9] <- sapply(UseData[,3:9], as.numeric)
+
+UseData[3] <- sapply(UseData[3], function(x) x/1000)
+
+# Convert dates and times and add new column date time
+
+UseData$datetime <- strptime(paste(UseData$Date, UseData$Time), "%Y-%m-%d %H:%M:%S")
+
+UseData$datetime <- as.POSIXct(UseData$datetime)
+
+# Create Rows and Columns
+
+par(mfrow=c(2,2))
+
+# Create Plot 1
+
+with (UseData, plot(Global_active_power ~ datetime, type = "l",
+                    ylab = "Global Active Power (kilowatts)", xlab = ""))
+# Create Plot 2
+
+with (UseData, plot(Voltage ~ datetime, type = "l",
+                    ylab = "Voltage", xlab = "datetime"))
+# Create Plot 3
+
+with(UseData, plot(Sub_metering_1 ~ datetime, type = "l", 
+                   ylab = "Energy sub metering", xlab = ""))
+# Add Blue lines
+with(UseData, lines(Sub_metering_2 ~ datetime, col = "Red"))
+
+#Add Red lines
+with(UseData, lines(Sub_metering_3 ~ datetime, col = "Blue"))
+
+#Add legend
+
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Create Plot 4
+
+with (UseData, plot(Global_reactive_power ~ datetime, type = "l",
+                    ylab = "Global_reactive_power", xlab = "datetime"))
+
+# Create PNG file
+
+png(file = "plot4.png", width = 480, height = 480, units = "px")
+
+par(mfrow=c(2,2))
+
+# Create Plot 1
+
+with (UseData, plot(Global_active_power ~ datetime, type = "l",
+                    ylab = "Global Active Power (kilowatts)", xlab = ""))
+# Create Plot 2
+
+with (UseData, plot(Voltage ~ datetime, type = "l",
+                    ylab = "Voltage", xlab = "datetime"))
+# Create Plot 3
+
+with(UseData, plot(Sub_metering_1 ~ datetime, type = "l", 
+                   ylab = "Energy sub metering", xlab = ""))
+# Add Blue lines
+with(UseData, lines(Sub_metering_2 ~ datetime, col = "Red"))
+
+#Add Red lines
+with(UseData, lines(Sub_metering_3 ~ datetime, col = "Blue"))
+
+#Add legend
+
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Create Plot 4
+
+with (UseData, plot(Global_reactive_power ~ datetime, type = "l",
+                    ylab = "Global_reactive_power", xlab = "datetime"))
+
++dev.off()
